08cf447b040f6eed461f6eb58877f08d2edebf25
블록체인/인증개발CoE 원은경 <togass@sk.com>
2021-09-15 09:27:28 +0900

    PodTest Project upload

---
+++ PodFrameworkTest/PodFrameworkTest/PodTest.swift
@@ -0,0 +1,17 @@
+//
+//  PodTest.swift
+//  PodFrameworkTest
+//
+//  Created by eunkyung won on 2021/09/14.
+//
+
+import Foundation
+
+class PodTest {
+    
+    public func printHello() {
+        
+        print("Pod Test Hellow")
+    }
+    
+}
---
+++ PodFrameworkTest/PodFrameworkTestTests/PodFrameworkTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  PodFrameworkTestTests.swift
+//  PodFrameworkTestTests
+//
+//  Created by eunkyung won on 2021/09/14.
+//
+
+import XCTest
+@testable import PodFrameworkTest
+
+class PodFrameworkTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
