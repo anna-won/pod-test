bb96a0af67059c455ec86b449f908657ab9e4800
블록체인/인증개발CoE 원은경 <togass@sk.com>
2021-09-27 10:22:22 +0900

    swift package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "PodTest",
+    name: "pod-test",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PodTest",
-            targets: ["PodTest", "PodFramework"]),
+            name: "pod-test",
+            targets: ["pod-test", "PodFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,12 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PodTest"
+            name: "pod-test"
         ),
         .binaryTarget(name: "PodFramework",
                       path: "Frameworks/PodFrameworkTest.xcframework"),
         .testTarget(
             name: "pod-testTests",
-            dependencies: ["PodTest"]),
+            dependencies: ["pod-test"]),
     ]
 )
