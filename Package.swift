ac40c17a7a7b220e4fd6e9d9828421793dc63ea6
블록체인/인증개발CoE 원은경 <togass@sk.com>
2021-09-27 14:49:31 +0900

    package.swit

--- Package.swift
+++ Package.swift
@@ -5,13 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "pod-test",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "pod-test",
-            targets: ["pod-test", "PodFramework"]),
+            targets: ["pod-test"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", from : "6.0.0"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.5"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "4.2.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -19,12 +25,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "pod-test"
+                    name: "pod-test",         // <--- new wrapper
+                    dependencies: [
+                        .target(name: "PodFramework")    // <-- reference the actual binary target here
+                    ],
+                    path: "TestSource",
+                    publicHeadersPath: ""
         ),
         .binaryTarget(name: "PodFramework",
                       path: "Frameworks/PodFrameworkTest.xcframework"),
-        .testTarget(
-            name: "pod-testTests",
-            dependencies: ["pod-test"]),
+       
     ]
 )
