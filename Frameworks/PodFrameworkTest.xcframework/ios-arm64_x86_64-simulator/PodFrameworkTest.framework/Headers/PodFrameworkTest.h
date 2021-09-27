40b32e8a5d0377819db5809f42c16a87c46c0ece
Anna <togass7@google.com>
2021-09-27 01:05:49 +0000

    Framework Add from gitAction

---
+++ Frameworks/PodFrameworkTest.xcframework/ios-arm64_x86_64-simulator/PodFrameworkTest.framework/Headers/PodFrameworkTest.h
@@ -0,0 +1,18 @@
+//
+//  PodFrameworkTest.h
+//  PodFrameworkTest
+//
+//  Created by eunkyung won on 2021/09/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodFrameworkTest.
+FOUNDATION_EXPORT double PodFrameworkTestVersionNumber;
+
+//! Project version string for PodFrameworkTest.
+FOUNDATION_EXPORT const unsigned char PodFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFrameworkTest/PublicHeader.h>
+
+
